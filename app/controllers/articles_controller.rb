e0e1394c31f160d6e258dd9bcb6b41d655d43174
Your Name <you@example.com>
2018-06-02 10:29:21 +0000

    ads

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 class ArticlesController < ApplicationController
 
   def index
+    @articles = Article.order(created_at: :desc)
   end
 
+  def new
+    @articles = Article.new
+    @series = Series.all
+  end
 
+  def create
+    @articles = Article.new(resource_params)
+    @articles.save
+    redirect_to root_path
+  end
 
-
-
+  def resource_params
+    params.require(:article).permit(:title, :content, :description, :keywords, :tags, :image, :series_id)
+  end
 
 end
