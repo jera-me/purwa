578d6850fc191cc9a685e6e6a33bb6e345a37368
Your Name <you@example.com>
2018-06-03 15:39:56 +0000

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require ckeditor/init
 //= require rails-ujs
 //= require jquery
+//= require jquery_ujs
 //= require turbolinks
-//= require ckeditor/init
 //= require_tree .
